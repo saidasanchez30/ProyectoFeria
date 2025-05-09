708e4c4fe803cc804a6c441cd70069d27f7695c0
saidasanchez30 <90401779+saidasanchez30@users.noreply.github.com>
2025-05-09 17:47:12 -0600

    Update CommonValues.h

--- CommonValues.h
+++ CommonValues.h
@@ -2,6 +2,6 @@
 #define COMMONVALS
 #include "stb_image.h"
 
-const int MAX_POINT_LIGHTS = 8;
-const int MAX_SPOT_LIGHTS = 8;
+const int MAX_POINT_LIGHTS = 7;
+const int MAX_SPOT_LIGHTS = 7;
 #endif
